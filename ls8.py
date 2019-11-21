f8c27e8f49e90da4aa819ded31b7c33c27aacbb8
Jarred Stanford <stanj89@gmail.com>
2019-11-21 17:40:37 -0500

    cors 2.0

--- ls8.py
+++ ls8.py
@@ -5,9 +5,11 @@
 import sys
 from cpu import *
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 # Instantiate our Node
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/binary', methods=['POST'])
